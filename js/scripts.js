81028071ef8b1779262ac02313320575cdb7441e
dcuthell <dcc1447@gmail.com>
2017-07-21 13:04:47 -0700

    add ability to add more pizzas to order, store pizza orders for future reference

--- js/scripts.js
+++ js/scripts.js
@@ -22,25 +22,61 @@ Store.prototype.addToOrder = function(pizza){
 
 Store.prototype.storeOrders = function(){
   while(this.currentOrder[0] !== undefined){
-    this.pastOrder.push(this.currentOrder[0]);
+    this.pastOrders.push(this.currentOrder[0]);
     this.currentOrder.pop();
   }
 }
 
+Pizza.prototype.calcPrice = function(){
+  var price = 10.00;
+  if(this.size === "medium"){
+    price += 3.50;
+  }
+  if(this.size === "large"){
+    price += 8.00;
+  }
+
+  return price;
+}
+
 //UI
 $(document).ready(function(){
+  store = new Store();
   $("#newOrder").click(function(){
     $("#openScreen").hide();
     $("#orderForm").show();
   });
-  $("#finishOrder").click(function(event){
+  $("#finishOrder").click(function(){
     $("#orderForm").hide();
     $("#customerInfo").show();
+    var size = $("input:radio[name=size]:checked").val();
     var toppings = [];
     $("input:checkbox:checked").map(function(){
       toppings.push($(this).val());
     });
-    alert(toppings[0] + toppings[1] + toppings[2]);
-    event.preventDefault;
+    $("input:radio[name=size]:checked").prop('checked', false);
+    $("input:checkbox:checked").prop('checked', false);
+    var thisPie = new Pizza(size, toppings, "defaultCustomer");
+    store.addToOrder(thisPie);
   });
+  $("#placeOrder").click(function(){
+    var name = $("input#customerName").val();
+    for(i = 0; i < store.currentOrder.length; i++){
+      store.currentOrder[i].customer = name;
+    }
+    console.log(store.currentOrder[0].size);
+    console.log(store.currentOrder[0].toppings);
+    console.log(store.currentOrder[0].customer);
+    store.storeOrders();
+    console.log(store.pastOrders[0].size);
+    console.log(store.pastOrders[0].toppings);
+    console.log(store.pastOrders[0].customer);
+    $("#openScreen").show();
+    $("#customerInfo").hide();
+  });
+  $("#morePizza").click(function(){
+    $("#customerInfo").hide();
+    $("#orderForm").show();
+  });
+
 });
