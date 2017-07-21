d05217de0dd2a94f665c0609439393b2fa93398d
dcuthell <dcc1447@gmail.com>
2017-07-21 10:55:47 -0700

    add constructors and prototypes to js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,33 @@
-//Back-End
+//Business Logic
+//Constructors
+function Store(){
+  this.currentOrder = [];
+  this.pastOrders = [];
+}
+
+function Pizza(size, toppings, customer){
+  this.size = size;
+  this.toppings = toppings;
+  this.customer = customer;
+}
+
+function Customer(name){
+  this.name = name;
+}
+
+//Prototypes
+Store.prototype.addToOrder = function(pizza){
+  this.currentOrder.push(pizza);
+}
+
+Store.prototype.storeOrders = function(){
+  while(this.currentOrder[0] !== undefined){
+    this.pastOrder.push(this.currentOrder[0]);
+    this.currentOrder.pop();
+  }
+}
 
 //UI
 $(document).ready(function(){
-  
+
 });
